39a474ae754e4100f050f95ba9155b1831dbd962
Matt Marincamp <mamarinc@1B5.42angouleme.fr>
2025-07-19 12:38:41 +0200

    nogame

--- frontend/src/components/Common/BackBtn.tsx
+++ frontend/src/components/Common/BackBtn.tsx
@@ -4,9 +4,9 @@ const BackBtn = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="flex-1 flex items-center ml-6">
+        <div className="flex-1 flex items-center">
             <button onClick={() => navigate(-1)} 
-            className="border-[2px] px-4 hover:scale-110 rounded-md bg-blue-800 h-[50px] w-[120px] flex items-center justify-center text-[4rem]">←</button>
+            className="border-[2px] px-4 hover:scale-110 rounded-md bg-blue-800 h-[50px] w-[120px] flex items-center justify-center text-[4rem] ml-6">←</button>
         </div>
     );
 };
