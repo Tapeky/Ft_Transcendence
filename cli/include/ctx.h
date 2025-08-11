167de6c27f6be5b2050e56b5bbaa34cf4ef1e321
dath <dath@dath.org>
2025-08-11 18:48:49 +0200

    CLI: include api_ctx and ws_ctx into ctx

--- cli/include/ctx.h
+++ cli/include/ctx.h
@@ -1,17 +1,21 @@
 #ifndef CTX_H
 # define CTX_H
 
-#include <X11/Xlib.h>
-#include "input.h"
+# include <X11/Xlib.h>
+# include "input.h"
+# include "api.h"
+# include "ws.h"
 
 typedef struct s_ctx
 {
 	Display		*dpy;
 	Window		root_win;
 	input_state	input;
+	ws_ctx		ws_ctx;
+	api_ctx		api_ctx;
 }   ctx;
 
-const char *ctx_init(ctx *ctx);
+int ctx_init(ctx *ctx, const char *api_endpoint_base, const char *ws_endpoint);
 void ctx_deinit(ctx *ctx);
 
 #endif
--- cli/src/ctx.c
+++ cli/src/ctx.c
@@ -1,25 +1,54 @@
 #include "ctx.h"
 
-const char *ctx_init(ctx *ctx)
+int ctx_init(ctx *ctx, const char *api_endpoint_base, const char *ws_endpoint)
 {
 	ctx->dpy = XOpenDisplay(NULL);
 	if (!ctx->dpy)
-		return ("Unable to open a display");
+	{
+		fprintf(stderr, "Unable to open a display\n");
+		return (0);
+	}
 
 	ctx->root_win = XDefaultRootWindow(ctx->dpy);
 	if (!input_init(ctx))
 	{
 		XCloseDisplay(ctx->dpy);
-		return ("Unable to grab keyboard");
+		ctx->dpy = NULL;
+		fprintf(stderr, "Unable to grab keyboard");
+		return (0);
 	}
-	return (NULL);
+
+	CURLcode curl_err = curl_global_init(CURL_GLOBAL_ALL);
+	if (curl_err)
+	{
+		fprintf(stderr, "curl_global_init() fail: %s\n", curl_easy_strerror(curl_err));
+		ctx_deinit(ctx);
+		return (0);
+	}
+	if (!api_ctx_init(&ctx->api_ctx, api_endpoint_base))
+	{
+		ctx_deinit(ctx);
+		return (0);
+	}
+	if (!ws_ctx_init(&ctx->ws_ctx, ws_endpoint))
+	{
+		ctx_deinit(ctx);
+		return (0);
+	}
+	return (1);
 }
 
 void ctx_deinit(ctx *ctx)
 {
+	if (!ctx)
+		return ;
 	if (ctx->dpy)
 	{
 		input_deinit(ctx);
 		XCloseDisplay(ctx->dpy);
+		ctx->dpy = NULL;
 	}
+	curl_global_cleanup();
+	ws_ctx_deinit(&ctx->ws_ctx);
+	api_ctx_deinit(&ctx->api_ctx);
 }
