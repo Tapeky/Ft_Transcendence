75d06a204f0994221c902e3fd0c485f7f6e400d9
dath <dath@dath.org>
2025-08-12 17:35:35 +0200

    CLI: reemplement basic interface with working login

--- cli/include/ctx.h
+++ cli/include/ctx.h
@@ -4,6 +4,7 @@
 # include <X11/Xlib.h>
 # include "input.h"
 # include "api.h"
+# include "term.h"
 # include "ws.h"
 
 typedef struct s_ctx
@@ -13,6 +14,14 @@ typedef struct s_ctx
 	input_state	input;
 	ws_ctx		ws_ctx;
 	api_ctx		api_ctx;
+
+	struct
+	{
+		console_component	*username_field;
+		console_component	*password_field;
+		console_component	*login_button;
+		console_component	*login_error_label;
+	};
 }   ctx;
 
 int ctx_init(ctx *ctx, const char *api_endpoint_base, const char *ws_endpoint);
